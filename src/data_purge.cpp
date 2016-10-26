7c9f947a4391635606ad03b03415faf959f3719b
Alessandro Fabbri <rommacc@yahoo.it>
2016-10-26 17:38:24 +0200

    add safety in parsing filter file, remove std::, keep last column unchanged

--- src/data_purge.cpp
+++ src/data_purge.cpp
@@ -32,8 +32,6 @@ along with Inertial Analysis. If not, see <http://www.gnu.org/licenses/>.
 
 #include "io_lib.hpp"
 
-using namespace std;
-
 #define MAJOR_VERSION       3
 #define MINOR_VERSION       0
 
@@ -43,17 +41,21 @@ class Filter {
 public:
   size_t first, last;
   size_t left, right;
-  std::vector<double> params;
+  vector<double> params;
 
-  Filter(const size_t& data_size, const std::string& filter_path) {
-    std::vector<double> coeff;
-    std::ifstream filter_file(filter_path);
+  Filter(const size_t& data_size, const string& filter_path) {
+    vector<double> coeff;
+    ifstream filter_file(filter_path);
     if (!filter_file) {
       cerr << "ERROR: file " << filter_path << " could not be opened" << endl;
       exit(1);
     }
     while (!filter_file.eof()) {
       double temp;
+      if (filter_file.fail()) {
+        cerr << "ERROR: Invalid filter data, check filter file " << filter_path << " and relaunch." << endl;
+        exit(2);
+      }
       filter_file >> temp;
       coeff.push_back(temp);
     }
@@ -70,7 +72,7 @@ public:
   };
 };
 
-std::ostream& operator<<(std::ostream &ost, const Filter &f) {
+ostream& operator<<(ostream &ost, const Filter &f) {
   ost << "first = " << f.first << "  last = " << f.last << "  size = " << f.params.size() << "  left = " << f.left << "  right = " << f.right;
   return ost;
 }
@@ -78,37 +80,37 @@ std::ostream& operator<<(std::ostream &ost, const Filter &f) {
 
 //////// MAIN
 void usage(char * progname) {
-  std::vector<std::string> tokens;
-  boost::split(tokens, std::string(progname), boost::is_any_of("/\\"));
-  std::cout << "Usage: " << tokens.back() << " -filter path/to/filter [-subtract] path/to/data/file" << std::endl;
-  std::cout << "       -filter create a filter from file" << std::endl;
-  std::cout << "       -subtract remove the average value of each column from the data [optional]" << std::endl;
-  std::cout << "       path/to/data/file must be a tab separated value file with timestamps in the first column" << std::endl;
+  vector<string> tokens;
+  boost::split(tokens, string(progname), boost::is_any_of("/\\"));
+  cout << "Usage: " << tokens.back() << " -filter path/to/filter [-subtract] path/to/data/file" << endl;
+  cout << "       -filter create a filter from file" << endl;
+  cout << "       -subtract remove the average value of each column from the data [optional]" << endl;
+  cout << "       path/to/data/file must be a tab separated value file with timestamps in the first column" << endl;
 }
 
 int main(int argc, char **argv) {
-  std::cout << "Data Purge v" << MAJOR_VERSION << "." << MINOR_VERSION << std::endl << std::endl;
+  cout << "Data Purge v" << MAJOR_VERSION << "." << MINOR_VERSION << endl << endl;
 
-  std::string input_file, filter_file;
+  string input_file, filter_file;
   int ma_sample = 10;
   bool subtract_ave = false;
   if (argc == 4 || argc == 5) {
     for (int i = 1; i < argc; i++) {
-      if (std::string(argv[i]) == "-filter") filter_file = argv[++i];
-      else if (std::string(argv[i]) == "-subtract") subtract_ave = true;
+      if (string(argv[i]) == "-filter") filter_file = argv[++i];
+      else if (string(argv[i]) == "-subtract") subtract_ave = true;
       else input_file = argv[i];
     }
   }
   else {
-    std::cout << "ERROR: Wrong command line parameters. Read usage and relaunch properly." << std::endl;
+    cout << "ERROR: Wrong command line parameters. Read usage and relaunch properly." << endl;
     usage(argv[0]);
     exit(-1);
   }
 
   if (input_file.substr(0, 2) == ".\\" || input_file.substr(0, 2) == "./") input_file = input_file.substr(2, input_file.size() - 2);
-  std::cout << "Purging        : " << input_file << std::endl;
-  std::cout << "Filter         : " << filter_file << std::endl;
-  std::cout << "Remove average : " << (subtract_ave ? "ON" : "OFF") << std::endl;
+  cout << "Purging         : " << input_file << endl;
+  cout << "Filter          : " << filter_file << endl;
+  cout << "Remove average  : " << (subtract_ave ? "ON" : "OFF") << endl;
 
   // Parse text file and convert it to vector of double
   vector< vector<string> > file_tokens;
@@ -117,7 +119,7 @@ int main(int argc, char **argv) {
   for (size_t i = 0; i < file_tokens.size(); i++) {
     vector<double> row;
     for (size_t j = 0; j < file_tokens[i].size(); j++) {
-      row.push_back(std::stod(file_tokens[i][j]));
+      row.push_back(stod(file_tokens[i][j]));
     }
     data.push_back(row);
   }
@@ -147,37 +149,40 @@ int main(int argc, char **argv) {
   }
 
   // Filtering
-  for (auto p : f.params) std::cout << p << " "; std::cout << std::endl;
-
   vector< vector<double> > data_purged;
   for (size_t i = f.first; i <= f.last; i++) {
     vector<double> row_purged;
-    row_purged.push_back(data[i][0]);                   // pushback timestamp without modification
-    for (size_t j = 1; j < data[0].size(); j++) {
+    row_purged.push_back(data[i][0]);                                // pushback timestamp without modification
+    for (size_t j = 1; j < data[0].size() - 1; j++) {
       double value_purged = 0.0;
       for (size_t k = i - f.left; k < i + f.right; k++) {
         value_purged += data[k][j] * f.params[k - i + f.left];       // params[...] the index is calculated to keep it in range
       }
-      row_purged.push_back(value_purged);
+      row_purged.push_back(value_purged);                            
     }
+    row_purged.push_back(data[i][data[0].size()-1]);                 // pushback relative_timestamp without modification
     data_purged.push_back(row_purged);
   }
-  std::cout << "Data original : r " << data.size() << " c " << data[0].size() << std::endl;
-  std::cout << "Data purged   : r " << data_purged.size() << " c " << data_purged[0].size() << std::endl;
+  cout << "Data original   : r " << data.size() << " c " << data[0].size() << endl;
+  cout << "Data purged     : r " << data_purged.size() << " c " << data_purged[0].size() << endl;
 
 
   // Dump to file
-  std::string out_file = input_file.substr(0, input_file.size() - 4) + "_purged" + (subtract_ave ? "_sub" : "") + ".txt";
-  std::ofstream output(out_file);
-  output << "# " << f << std::endl;
+  string out_file = input_file.substr(0, input_file.size() - 4) + "_purged" + (subtract_ave ? "_sub" : "") + ".txt";
+  ofstream output(out_file);
+  output << "# Filter Data\n# " << f << endl;
+  output << "# coefficients : "; for (auto p : f.params) output << p << "  "; output << endl;
+  output << "#" << endl;
+  output << "#  timestamp # speed #     ax #     ay #     az #     gx #      gy #     gz #   |a| #  timestamp_rel #" << std::endl;
+  output << "#            #       #        #        #        #        #         #        #       #                #" << std::endl;
   for (size_t i = 0; i < data_purged.size(); i++) {
     for (size_t j = 0; j < data_purged[i].size(); j++) {
-      output << std::fixed << std::setprecision(6) << data_purged[i][j] << "\t";
+      output << fixed << setprecision(6) << data_purged[i][j] << "\t";
     }
-    output << std::endl;
+    output << endl;
   }
   output.close();
-  std::cout << "Purge result in : " << out_file << std::endl;
+  cout << "Purge result in : " << out_file << endl;
 
   return 0;
 }
